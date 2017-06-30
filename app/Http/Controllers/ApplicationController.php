22c6c946e55612096a5c3053d232b109a4c105f9
Dmitry Punk <dmitry8912@gmail.com>
2017-06-30 11:23:55 +0400

    Appending new address requests into db

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\DB;
 
 class ApplicationController extends Controller
 {
     public function sendApplication(Request $request) {
-        Mail::send('mail/new_address', $request, function($message) {
+        Mail::send('mail/new_address', [
+            'houseNumber' => $request['new-house'],
+            'streetName' => $request['new-street'],
+            'fullAddress' => $request['comments']
+        ], function($message) {
             $message->to(env('MAIL_USERNAME'))
                 ->subject('FIAS: NEW ADDRESS');
         });
-        DB::statement('INSERT INTO "requests" values(DEFAULT,{$request["person_id"]},{$request["new-district"]},{$request["new-region"]},{$request["new-city"]},{$request["new-street"]},{$request["new-house"]},{$request["comments"]})');
+        DB::statement("INSERT INTO requests values(DEFAULT,".$request['person_id'].",".
+            $request['new-district'].",".$request['new-region'].",".$request['new-city'].",".$request['new-street'].",".
+            $request['new-house'].",".$request['comments']);
     }
 }
